de5a122441f2aaee97b7b291c5e8f1119a0ff12f
rohitmodh <rmodh21@gmail.com>
2025-04-18 21:33:09 +0530

    new change git try

--- OnlineShopping/kafka/OrderProducer.js
+++ OnlineShopping/kafka/OrderProducer.js
@@ -33,6 +33,8 @@ const ensureTopicExists = async (topicName) => {
     //this is code to test git webhook and api
     console.log(`✅ Kafka topic exists: ${topicName}`);
     console.log(`✅ Kafka topic git change exists: ${topicName}`);
+    
+    console.log(`✅ Kafka topic git change exists: ${topicName}`);
   }
 };
 
